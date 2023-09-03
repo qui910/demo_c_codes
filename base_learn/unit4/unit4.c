fddff188e0c9ab5dfab43e96f30a18617cac85d1
qui910 <qui910@163.com>
2023-09-03 18:01:15 +0800

    明解C语音第4章笔记

--- base_learn/unit4/unit4.c
+++ base_learn/unit4/unit4.c
@@ -5,5 +5,83 @@
 
 int main(void)
 {
+//    loop_do_base();
+//    loop_while_base();
+//    loop_while_reverse_int();
+//    loop_for_base();
+    loop_for_condition_c_base();
     return 0;
+}
+
+// do语句基础使用
+void loop_do_base() {
+    int retry;
+    do {
+        int no;
+        printf("请输入一个整数：");
+        scanf("%d",&no);
+        if (no % 2)
+            puts("这个整数是奇数");
+        else
+            puts("这个整数是偶数");
+
+        printf("是否重复?【Yes...0 / No...9】:");
+        scanf("%d",&retry);
+    } while (retry==0);
+}
+
+// while语句基础使用
+void loop_while_base()
+{
+    int no;
+    printf("请输入一个正整数：");
+    scanf("%d",&no);
+
+    while(--no>=0) {
+//        printf("%d ",no);
+        putchar('*');
+    }
+//    printf("\n");
+    putchar('\n');
+}
+
+// 将输入的正整数倒序显示 eg:1963 显示未3691
+void loop_while_reverse_int() {
+    int no;
+    do {
+        printf("请输入一个正整数：");
+        scanf("%d", &no);
+        if (no <= 0)
+            puts("\a 请不要输入非正整数");
+    } while (no <=0);
+
+    printf("该正整数逆向显示的结果是");
+    while(no >0) {
+        printf("%d", no % 10);
+        no /= 10;
+    }
+    puts("--end");
+}
+
+// for循环基础
+void loop_for_base()
+{
+    int i,no;
+    printf("请输入一个正整数：");
+    scanf("%d", &no);
+    for (i=0;i<no;i++)
+        printf("%d ",i);
+    puts("\n");
+}
+
+// 显示输入的整数值以下的偶数
+void loop_for_condition_c_base()
+{
+    int i,n;
+    printf("整数值：");
+    scanf("%d", &n);
+
+    for (i = 2; i < n; i+=2)
+        printf("%d ",i);
+    putchar('\n');
 }
--- base_learn/unit4/unit4.h
+++ base_learn/unit4/unit4.h
@@ -5,5 +5,9 @@
 
 #ifndef DEMO_C_CODES_UNIT4_H
 #define DEMO_C_CODES_UNIT4_H
-
+void loop_do_base();
+void loop_while_base();
+void loop_while_reverse_int();
+void loop_for_base();
+void loop_for_condition_c_base();
 #endif //DEMO_C_CODES_UNIT4_H
